56cd5c6c7ebbb0c3be23d15a7097efa18daf81fe
ShuoX Liu <shuox.liu@intel.com>
2010-12-28 16:52:36 +0800

    Add TI wlan1283 HAL support on MFLD ES2
    Change-Id: I49492059f9ee4f8b673d93eb9244468548de4121

---
+++ wifi/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation, not prelinked and stored in
+# hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+ifneq (,$(findstring $(CUSTOM_BOARD),mfld_cdk))
+
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libnetutils libwpa_client
+LOCAL_SRC_FILES += wifi.c
+LOCAL_C_INCLUDES += \
+        $(call include-path-for, libhardware)/hardware
+
+ifeq ($(BOARD_HAVE_TI12XX),true)
+LOCAL_CFLAGS += -DTIWLAN
+endif
+
+LOCAL_MODULE := wifi.$(TARGET_DEVICE)
+include $(BUILD_SHARED_LIBRARY)
+endif
+

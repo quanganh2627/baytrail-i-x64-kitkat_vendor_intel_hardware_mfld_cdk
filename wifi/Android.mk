4680ead3e6b72f5226c92bb6f045117f0ccc8c86
Jean-Michel.Bachot <jean-michelx.bachot@intel.com>
2011-04-18 11:22:03 +0200

    wifi_nlcp: Added new HAL glue layer file , named wifi_nlcp.c, for NLCP
    BZ: 760
    This file is the HAL glue layer developed for the TI wl12xx driver.
    
    Change-Id: I78f234730749b1423268201cf62fd52dc27818a3

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -9,13 +9,15 @@ LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libnetutils libwpa_client
+ifeq ($(BOARD_WLAN_DEVICE),wl1283)
 LOCAL_SRC_FILES += wifi.c
+endif
+ifeq ($(BOARD_WLAN_DEVICE),wl12xx-compat)
+LOCAL_SRC_FILES += wifi_nlcp.c
+endif
 LOCAL_C_INCLUDES += \
         $(call include-path-for, libhardware)/hardware
 
-ifeq ($(BOARD_HAVE_TI12XX),true)
-LOCAL_CFLAGS += -DTIWLAN
-endif
 
 LOCAL_MODULE := wifi.$(TARGET_DEVICE)
 LOCAL_MODULE_TAGS := optional

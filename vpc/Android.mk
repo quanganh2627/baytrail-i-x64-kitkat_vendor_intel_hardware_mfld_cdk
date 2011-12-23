ddf50637b6df4dfbd76f659c25f5da9def9cb0af
karim bourahmani <karimx.bourahmani@intel.com>
2011-12-23 15:45:00 +0100

    Add GI variant
    BZ: 18247
    
    Change-Id: I85fd74356a076ada8d7fefbda65b8399b482f3a4
    Signed-off-by: karim bourahmani <karimx.bourahmani@intel.com>
    Reviewed-on: http://android.intel.com:8080/29856
    Reviewed-by: Monnier, OlivierX <olivierx.monnier@intel.com>
    Tested-by: Seibel, Eric <eric.seibel@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- vpc/Android.mk
+++ vpc/Android.mk
@@ -1,18 +1,18 @@
 
 LOCAL_PATH := $(call my-dir)
 
-  include $(CLEAR_VARS)
+include $(CLEAR_VARS)
 
-  LOCAL_PRELINK_MODULE := false
+LOCAL_PRELINK_MODULE := false
 
-  LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
-  LOCAL_COPY_HEADERS_TO := hw
+LOCAL_COPY_HEADERS_TO := hw
 
-  LOCAL_COPY_HEADERS := \
+LOCAL_COPY_HEADERS := \
     vpc_hardware.h
 
-  LOCAL_CFLAGS := -D_POSIX_SOURCE -Wno-multichar
+LOCAL_CFLAGS := -D_POSIX_SOURCE -Wno-multichar
 
 ifeq ($(CUSTOM_BOARD),mfld_pr2)
     LOCAL_CFLAGS += -DCUSTOM_BOARD_WITH_AUDIENCE
@@ -22,36 +22,39 @@ ifeq ($(CUSTOM_BOARD),ctp_pr0)
      LOCAL_CFLAGS += -DCUSTOM_BOARD_WITH_AUDIENCE
 endif
 
+ifeq ($(CUSTOM_BOARD),mfld_gi)
+    LOCAL_CFLAGS += -DCUSTOM_BOARD_WITH_AUDIENCE
+endif
 
 ifneq ($(ALSA_DEFAULT_SAMPLE_RATE),)
     LOCAL_CFLAGS += -DALSA_DEFAULT_SAMPLE_RATE=$(ALSA_DEFAULT_SAMPLE_RATE)
 endif
 
-  LOCAL_C_INCLUDES += \
-        external/alsa-lib/include \
-        hardware/intel/include \
-        hardware/intel/mfld_cdk/vpc \
-        hardware/intel/mfld_cdk/libamc \
-        system/core/include/cutils \
-        external/bluetooth/bluez/lib/bluetooth \
-        hardware/intel/IFX-modem
+LOCAL_C_INCLUDES += \
+     external/alsa-lib/include \
+     hardware/intel/include \
+     hardware/intel/mfld_cdk/vpc \
+     hardware/intel/mfld_cdk/libamc \
+     system/core/include/cutils \
+     external/bluetooth/bluez/lib/bluetooth \
+     hardware/intel/IFX-modem
 
-  LOCAL_SRC_FILES:= \
-        ctl_vpc.cpp \
-        acoustic.cpp \
-        bt.cpp \
-        msic.cpp \
-        volume_keys.cpp
+LOCAL_SRC_FILES:= \
+     ctl_vpc.cpp \
+     acoustic.cpp \
+     bt.cpp \
+     msic.cpp \
+     volume_keys.cpp
 
-  LOCAL_SHARED_LIBRARIES := \
-        libasound \
-        liblog \
-        libbluetooth \
-        libcutils \
-        libamc
+LOCAL_SHARED_LIBRARIES := \
+     libasound \
+     liblog \
+     libbluetooth \
+     libcutils \
+     libamc
 
-  LOCAL_MODULE:= vpc.$(TARGET_DEVICE)
-  LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE:= vpc.$(TARGET_DEVICE)
+LOCAL_MODULE_TAGS := optional
 
-  include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
 

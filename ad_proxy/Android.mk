01815a9e762d6bbce49d50512d78d77361b3bf0e
Mathieu Soulard <mathieux.soulard@intel.com>
2012-04-23 15:10:09 +0200

    [Audio] Audience proxy
    BZ: 27795
    
    This is the Audience proxy for debug images.
    
    It allows to interact with the Audience chip from a PC host with the
    tuning tool AuViD provided by Audience.
    
    Change-Id: I0cbb36070545f3e7f8e0ce02eb6ee5b834eb2ac9
    Signed-off-by: Mathieu Soulard <mathieux.soulard@intel.com>
    Reviewed-on: http://android.intel.com:8080/44463
    Reviewed-by: Fomitchev, AndreiX <andreix.fomitchev@intel.com>
    Reviewed-by: Centelles, Sylvain <sylvain.centelles@intel.com>
    Tested-by: Mendi, EduardoX <eduardox.mendi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ ad_proxy/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH:= $(call my-dir)
+
+common_c_includes := \
+	$(KERNEL_HEADERS) \
+
+common_shared_libraries := \
+	libsysutils \
+	libcutils
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := proxy_main.c ad_i2c.c ad_protocol.c ad_usb_tty.c
+
+LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
+LOCAL_MODULE := ad_proxy
+LOCAL_MODULE_TAGS := debug
+
+include $(BUILD_EXECUTABLE)
+

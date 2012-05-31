c2d44a3f0b61ca53cf8fae63cad5543a73d7eeca
Francois Gaffie <francois.gaffie@intel.com>
2012-05-31 17:34:43 +0200

    [PORT FROM R3][AUDIO][LEX][BT] BT HSP does not work on VoIP during swap CSV->VoIP
    BZ: 37504
    
    Lex does not have audience chip. VoIP path is now routed on "media"
    route ie on PCM2 of MSIC except for BT SCO.
    
    Filtering the INCOMM mode was a mistake as it did not allow to set
    the audio path on BT in case of swap.
    
    Change-Id: Ibde2050959f464e48fc7b2e9a48a91a547b482f5
    Orig-Change-Id: I292ef98c086f77f7720fe1df56f27d84376818b9
    Signed-off-by: Francois Gaffie <francois.gaffie@intel.com>
    Reviewed-on: http://android.intel.com:8080/54605
    Reviewed-by: Mendi, EduardoX <eduardox.mendi@intel.com>
    Tested-by: Mendi, EduardoX <eduardox.mendi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- vpc/ctl_vpc.cpp
+++ vpc/ctl_vpc.cpp
@@ -166,17 +166,7 @@ static int vpc_params(int mode, uint32_t device)
 {
     vpc_lock.lock();
 
-#ifndef CUSTOM_BOARD_WITH_AUDIENCE
-    //
-    // Hack the mode here. Do not want to handle any configuration on VPC in
-    // communication mode.
-    // Default configuration allows BT SCO, which will be used for incommunication
-    // on BT SCO.
-    //
-    current_mode =  mode == AudioSystem::MODE_IN_COMMUNICATION ? AudioSystem::MODE_NORMAL : mode;
-#else
     current_mode = mode;
-#endif
     current_device = device;
 
     LOGD("vpc_params mode = %d device = %d\n", current_mode, current_device);
@@ -193,17 +183,7 @@ static void vpc_set_mode(int mode)
 {
     vpc_lock.lock();
 
-#ifndef CUSTOM_BOARD_WITH_AUDIENCE
-    //
-    // Hack the mode here. Do not want to handle any configuration on VPC in
-    // communication mode.
-    // Default configuration allows BT SCO, which will be used for incommunication
-    // on BT SCO.
-    //
-    current_mode =  mode == AudioSystem::MODE_IN_COMMUNICATION ? AudioSystem::MODE_NORMAL : mode;
-#else
     current_mode = mode;
-#endif
 
     LOGD("%s: mode = %d\n", __FUNCTION__, current_mode);
     LOGD("%s: previous mode = %d\n", __FUNCTION__, prev_mode);

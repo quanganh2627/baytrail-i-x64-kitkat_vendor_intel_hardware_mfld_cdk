1c9bf56d6161dc4b540f3cca2adb69d3322b2f11
zhang dongming <dongmingx.zhang@intel.com>
2012-06-06 13:25:36 +0800

    [PORT FROM R3] hwc: modify the Copyright declaration in hwc module
    BZ: 40108
    
    Copyright declaration in hwc module is "Android source", We should
    change them to Intel Copyright .
    
    Change-Id: Iac5df64463496829660519affd8dc5005a4bd407
    Signed-off-by: zhang dongming <dongmingx.zhang@intel.com>
    Reviewed-on: http://android.intel.com:8080/51666
    Tested-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Xu, Randy <randy.xu@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- hwc/libhwcwidi/HwWidiPlaneProxy.cpp
+++ hwc/libhwcwidi/HwWidiPlaneProxy.cpp
@@ -1,39 +1,27 @@
 /*
-**
-** Copyright 2008, The Android Open Source Project
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
+* Copyright Â© 2012 Intel Corporation
+* All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice (including the next
+* paragraph) shall be included in all copies or substantial portions of the
+* Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*
 */
-
-/*
- * INTEL CONFIDENTIAL
- * Copyright 2010-2011 Intel Corporation All Rights Reserved.
-
- * The source code, information and material ("Material") contained herein is owned
- * by Intel Corporation or its suppliers or licensors, and title to such Material
- * remains with Intel Corporation or its suppliers or licensors. The Material contains
- * proprietary information of Intel or its suppliers and licensors. The Material is
- * protected by worldwide copyright laws and treaty provisions. No part of the Material
- * may be used, copied, reproduced, modified, published, uploaded, posted, transmitted,
- * distributed or disclosed in any way without Intel's prior express written permission.
- * No license under any patent, copyright or other intellectual property rights in the
- * Material is granted to or conferred upon you, either expressly, by implication, inducement,
- * estoppel or otherwise. Any license under such intellectual property rights must be express
- * and approved by Intel in writing.
-
- * Unless otherwise agreed by Intel in writing, you may not remove or alter this notice or any
- * other notice embedded in Materials by Intel or Intel's suppliers or licensors in any way.
- */
 
 #include <stdint.h>
 #include <sys/types.h>

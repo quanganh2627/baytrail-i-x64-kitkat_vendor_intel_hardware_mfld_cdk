318da766ec089e0d12cda9339cd66e9321299322
Muthukumar K <muthukumar.kandasamy@intel.com>
2012-07-25 16:07:47 +0545

    [JellyBean] Enable WiDi support in Audio middleware
    BZ: 48525
    
    Widi Support is enabled in the audio Middleware path
    and audio HAL. This doesnot include Widi stack.
    
    Change-Id: If3ffbf9d571ddcd7b3d7fb2fef500dc9099b278b
    Signed-off-by: Muthukumar K <muthukumar.kandasamy@intel.com>
    Reviewed-on: http://android.intel.com:8080/58444
    Reviewed-by: Sikkandar D, Madar <madar.sikkandar.d@intel.com>
    Tested-by: S, Kumaran <kumaran.s@intel.com>
    Reviewed-by: Nallasellan, Singaravelan <singaravelan.nallasellan@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- alsa/alsa_mfld_cdk.cpp
+++ alsa/alsa_mfld_cdk.cpp
@@ -162,7 +162,7 @@ static const device_suffix_t deviceSuffix[] = {
     { AudioSystem::DEVICE_OUT_WIRED_HEADSET,         "_Headset" },
     { AudioSystem::DEVICE_OUT_BLUETOOTH_A2DP,        "_Bluetooth-A2DP" },
     { AudioSystem::DEVICE_OUT_AUX_DIGITAL,           "_HDMI" },
-    { AudioSystem::DEVICE_OUT_WIDI_LOOPBACK,         "_Widi-Loopback" },
+    { AudioSystem::DEVICE_OUT_WIDI,                  "_Widi" },
     { AudioSystem::DEVICE_OUT_DEFAULT,                "_Null" },
     { AudioSystem::DEVICE_IN_BUILTIN_MIC,            "_BuiltinMic" },
     { AudioSystem::DEVICE_IN_BACK_MIC,               "_BuiltinBackMic" },
@@ -611,7 +611,7 @@ static status_t s_open(alsa_handle_t *handle, uint32_t devices, int mode, int fm
             handle->sampleRate = VOICE_CODEC_DEFAULT_SAMPLE_RATE;
 #endif
         }
-        else if (devices & AudioSystem::DEVICE_OUT_WIDI_LOOPBACK) {
+        else if (devices & AudioSystem::DEVICE_OUT_WIDI) {
 
             handle->latency = WIDI_DEFAULT_LATENCY;
         }

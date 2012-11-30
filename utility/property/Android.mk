ea0b734f4d715a8cdb0e83ab2f6a42d2567364bf
Jeremie Graulle <jeremiex.graulle@intel.com>
2012-11-30 21:02:33 +0100

    [PORT FROM MAIN][property] Add a build entry for host test
    BZ: 65646
    
    Add a static build entry in Android.mk with gcov for host unit test.
    This static lib opposite to share lib permit to use mock lib instead of
    real Android lib.
    
    Change-Id: I9263f2e3bfc6bda64ea27b3f0ea48cb5b1a0e9c3
    Origin-Change-Id: I2b9755d66a3a2ef40d9ed022e8d5358d1934db1f
    Signed-off-by: Jeremie Graulle <jeremiex.graulle@intel.com>
    Reviewed-on: http://android.intel.com:8080/81276
    Reviewed-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Tested-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- utility/property/Android.mk
+++ utility/property/Android.mk
@@ -1,33 +1,32 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_COPY_HEADERS_TO := property
-LOCAL_COPY_HEADERS := \
+# common variables
+##################
+
+property_exported_includes_folder := property
+property_exported_includes_files := \
     PropertyBase.h \
     Property.h
-include $(BUILD_COPY_HEADERS)
-include $(CLEAR_VARS)
-LOCAL_COPY_HEADERS_TO := property
-LOCAL_COPY_HEADERS := \
-    Property.h \
-    PropertyBase.h
-include $(BUILD_COPY_HEADERS)
+
+property_src_files := \
+    PropertyBase.cpp \
+    Property.cpp
+
+
+# build for target
+##################
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS := \
-        -DDEBUG
+LOCAL_COPY_HEADERS_TO := $(property_exported_includes_folder)
+LOCAL_COPY_HEADERS := $(property_exported_includes_files)
 
-LOCAL_SRC_FILES := \
-        PropertyBase.cpp \
-        Property.cpp
+LOCAL_CFLAGS := -DDEBUG
+
+LOCAL_SRC_FILES := $(property_src_files)
 
 LOCAL_C_INCLUDES += \
-        system/core/include/cutils
-
-LOCAL_C_INCLUDES += \
-        external/stlport/stlport/ \
-        bionic/libstdc++ \
-        bionic/
+    external/stlport/stlport \
+    bionic
 
 LOCAL_SHARED_LIBRARIES := libstlport libcutils
 
@@ -37,3 +36,23 @@ LOCAL_MODULE_TAGS := optional
 TARGET_ERROR_FLAGS += -Wno-non-virtual-dtor
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+# build for host test
+#####################
+
+include $(CLEAR_VARS)
+
+LOCAL_COPY_HEADERS_TO := $(property_exported_includes_folder)
+LOCAL_COPY_HEADERS := $(property_exported_includes_files)
+
+LOCAL_SRC_FILES := $(property_src_files)
+
+LOCAL_CFLAGS += -O0 -fprofile-arcs -ftest-coverage
+LOCAL_LDFLAGS += -fprofile-arcs -lgcov
+
+LOCAL_MODULE := libproperty_static_host
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_STATIC_LIBRARY)
+

3784f13f0c2a4bcd2cbe409c5f345e9347f473fb
Thierry Quervel <thierry.quervel@intel.com>
2013-01-16 15:21:04 +0100

    [AUDIO] Add Audience eS325 FW channel IDs support
    BZ: 80160
    
    When doing a capture with ad_streamer, you need to specify
    the channel IDs to capture (prim_mic, clean_speech, ...).
    
    With new Audience eS325 FW, some channel IDs have changed,
    compared to the eS305 FW values.
    So, we need to align ad_streamer with old and new channel ID
    values. Choice of eS305 ID set of eS325 ID set is based at
    run-time on "audiocomms.vp.fw_name" property.
    
    Change-Id: I972b0547a5d237d76282f03232aec2016cbaba38
    Signed-off-by: Thierry Quervel <thierry.quervel@intel.com>
    Reviewed-on: http://android.intel.com:8080/87123
    Reviewed-by: Becker, VincentX <vincentx.becker@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Soulard, Mathieu <mathieu.soulard@intel.com>
    Reviewed-by: De Chivre, Renaud <renaud.de.chivre@intel.com>
    Reviewed-by: Lee, JhinX <jhinx.lee@intel.com>
    Tested-by: Dixon, CharlesX <charlesx.dixon@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- ad_streamer/ad_streamer.c
+++ ad_streamer/ad_streamer.c
@@ -28,10 +28,12 @@
 #include <sys/select.h>
 #include <sys/ioctl.h>
 #include <semaphore.h>
+#include <cutils/properties.h>
+#include <ctype.h>
 
 #include "hardware_legacy/power.h"
 
-#define ES305_DEVICE_PATH	"/dev/audience_es305"
+#define ES3X5_DEVICE_PATH	"/dev/audience_es305"
 
 #define ES305_CH_PRI_MIC	0x1
 #define ES305_CH_SEC_MIC	0x2
@@ -39,16 +41,38 @@
 #define ES305_CH_FAR_END_IN	0x8
 #define ES305_CH_FAR_END_OUT	0x10
 
-#define ES305_CHANNEL_SELECT_FIELD 3
+#define ES325_CH_PRI_MIC		0x1
+#define ES325_CH_SEC_MIC		0x2
+#define ES325_CH_THIRD_MIC		0x3
+#define ES325_CH_FOURTH_MIC		0x4
+#define ES325_CH_FAR_END_INPUT		0x5
+#define ES325_CH_AUDIO_INPUT_1		0x6
+#define ES325_CH_AUDIO_INPUT_2		0x7
+#define ES325_CH_AUDIO_INPUT_3		0x8
+#define ES325_CH_AUDIO_INPUT_4		0x9
+#define ES325_CH_UI_TONE_1			0xA
+#define ES325_CH_UI_TONE_2			0xB
+#define ES325_CH_SPK_REF_1			0xC
+#define ES325_CH_SPK_REF_2			0xD
+#define ES325_CH_CLEAN_SPEECH	0x10
+#define ES325_CH_FAR_END_OUT_1	0x11
+#define ES325_CH_FAR_END_OUT_2	0x12
+#define ES325_CH_AUDIO_OUTPUT_1	0x13
+#define ES325_CH_AUDIO_OUTPUT_2	0x14
+#define ES325_CH_AUDIO_OUTPUT_3	0x15
+#define ES325_CH_AUDIO_OUTPUT_4	0x16
+
+#define ES3X5_CHANNEL_SELECT_FIELD 3
 
 #define DEFAULT_OUTPUT_FILE	"/data/a1026_stream.bin"
+#define DEFAULT_CAPTURE_DURATION_IN_SEC 5
 
-//Total byte of buffer size would be ES305_BUFFER_SIZE * ES305_BUFFER_COUNT
+//Total byte of buffer size would be ES3X5_BUFFER_SIZE * ES3X5_BUFFER_COUNT
 //Audience send 328 bytes for each frame. 164 bytes are half of the frame size.
 //It also need to be less than 256 bytes as the limitation of I2C driver.
-#define ES305_BUFFER_SIZE		164
-/* ES305_BUFFER_COUNT must be a power of 2 */
-#define ES305_BUFFER_COUNT		256
+#define ES3X5_BUFFER_SIZE		164
+/* ES3X5_BUFFER_COUNT must be a power of 2 */
+#define ES3X5_BUFFER_COUNT		256
 
 #define false	0
 #define true	1
@@ -68,18 +92,40 @@
 //Waiting until all audience stream is flushed in micro seconds
 #define THREAD_EXIT_WAIT_IN_US 100000
 
+typedef enum {
+	AUDIENCE_ES305,
+	AUDIENCE_ES325,
+	AUDIENCE_UNKNOWN
+} fw_version_t;
+
+// Private data
 static const unsigned char startStreamCmd[4] = { 0x80, 0x25, 0x00, 0x01 };
 static const unsigned char stopStreamCmd[4] = { 0x80, 0x25, 0x00, 0x00 };
 static const unsigned char setOutputKnownSignal[4] = { 0x80, 0x1E, 0x00, 0x05 };
 static unsigned char setChannelCmd[4] = { 0x80, 0x28, 0x00, 0x03 };
 
-// Globals.....
+static const char *    vp_fw_name_prop_name = "audiocomms.vp.fw_name";
+static const char *    vp_fw_name_prop_default_value = "Unknown";
+static char            vp_fw_name[PROPERTY_VALUE_MAX];
+
+static const char* fwVersion2String[] = {
+	"es305",
+	"es325",
+	"Unknown"
+};
+
+static fw_version_t fw_version = AUDIENCE_UNKNOWN;
+
 static int fd = -1;
 static FILE *outfile = NULL;
 static volatile int stop_requested = false;
 static unsigned int written_bytes = 0;
 static unsigned int read_bytes = 0;
-static unsigned char data[ES305_BUFFER_COUNT][ES305_BUFFER_SIZE];
+static unsigned char data[ES3X5_BUFFER_COUNT][ES3X5_BUFFER_SIZE];
+static int duration_in_sec = DEFAULT_CAPTURE_DURATION_IN_SEC;
+static int channel_flag = 0;
+static char fname[MAX_FILE_PATH_SIZE] = DEFAULT_OUTPUT_FILE;
+
 
 static sem_t sem_read;
 static sem_t sem_start;
@@ -88,6 +134,27 @@ static char lockid[32];
 
 static volatile long int cap_idx = 0;
 static volatile long int wrt_idx = 0;
+
+int get_fw_version(){
+	if (fw_version != AUDIENCE_UNKNOWN){
+		// chip selection has been forced as cmd line argument
+		// keep it.
+		printf("Audience chip forced to '%s'.\n", fwVersion2String[fw_version]);
+		return 0;
+	}
+	property_get(vp_fw_name_prop_name, vp_fw_name, vp_fw_name_prop_default_value);
+	if (strstr(vp_fw_name,"es325") != NULL){
+		fw_version = AUDIENCE_ES325;
+		// According eS325 specifications, bit #15 of commands must be set to 1
+		setChannelCmd[2] = 0x80;
+	} else if (strstr(vp_fw_name,"es305") != NULL){
+		fw_version = AUDIENCE_ES305;
+	} else {
+		fw_version = AUDIENCE_UNKNOWN;
+	}
+	printf("Audience chip detected: '%s'.\n", fwVersion2String[fw_version]);
+	return 0;
+};
 
 // Thread to keep track of time
 void* run_capture(void *ptr) {
@@ -103,16 +170,16 @@ void* run_capture(void *ptr) {
 		return NULL;
 	}
 	while (!stop_requested) {
-		read(fd, data[index++], ES305_BUFFER_SIZE);
+		read(fd, data[index++], ES3X5_BUFFER_SIZE);
 		cap_idx++;
-		if(UINT_MAX - read_bytes >= ES305_BUFFER_SIZE)
-			read_bytes += ES305_BUFFER_SIZE;
+		if(UINT_MAX - read_bytes >= ES3X5_BUFFER_SIZE)
+			read_bytes += ES3X5_BUFFER_SIZE;
 		else
 			printf("read bytes are overflowed.\n");
-		index &= ES305_BUFFER_COUNT-1;
+		index &= ES3X5_BUFFER_COUNT-1;
 		buffer_level = cap_idx + wrt_idx;
-		if(buffer_level > ES305_BUFFER_COUNT){
-			printf("Warning: buffer overflow (%d/%d)\n", buffer_level, ES305_BUFFER_COUNT);
+		if(buffer_level > ES3X5_BUFFER_COUNT){
+			printf("Warning: buffer overflow (%d/%d)\n", buffer_level, ES3X5_BUFFER_COUNT);
 		}
 		sem_post(&sem_read);
 	}
@@ -133,13 +200,13 @@ void* run_writefile(void *ptr) {
 			}
 			return NULL;
 		}
-		fwrite(data[index++], sizeof(unsigned char),ES305_BUFFER_SIZE, outfile);
+		fwrite(data[index++], sizeof(unsigned char),ES3X5_BUFFER_SIZE, outfile);
 		wrt_idx--;
-		if(UINT_MAX - written_bytes >= ES305_BUFFER_SIZE)
-			written_bytes+=ES305_BUFFER_SIZE;
+		if(UINT_MAX - written_bytes >= ES3X5_BUFFER_SIZE)
+			written_bytes+=ES3X5_BUFFER_SIZE;
 		else
 			printf("written bytes are overflowed\n");
-		index &= ES305_BUFFER_COUNT-1;
+		index &= ES3X5_BUFFER_COUNT-1;
 	}
 	return NULL;
 }
@@ -162,24 +229,109 @@ int check_numeric(char *number){
 	return 0;
 }
 
-int display_help(){
-	printf("Format: [flag] [seconds] [optional:output file path/filename]\n");
-	printf("flag = channel options:\n");
+void display_help(){
+	printf("Format: [-t seconds] [-f /path/filename] [-c chip] channelId \n");
+	printf("Channel IDs for %s\n", fwVersion2String[AUDIENCE_ES305]);
 	printf("	Primary Mic   = %d\n", ES305_CH_PRI_MIC);
 	printf("	Secondary Mic = %d\n", ES305_CH_SEC_MIC);
 	printf("	Clean Speech  = %d\n", ES305_CH_CLEAN_SPEECH);
 	printf("	Far End In    = %d\n", ES305_CH_FAR_END_IN);
 	printf("	Far End out   = %d\n", ES305_CH_FAR_END_OUT);
-	printf("Usage of flag: Capture Primary Mic and Far End In, set FLAG to 9 (1 + 8)\n");
-	printf("Default output file path is /data/a1026_stream.bin\n");
-	printf("Example: ad_streamer 9 10 /sdcard/aud_stream.bin\n");
-	return 0;
+	printf("	Example: ad_streamer -c es305 -t 10 -f /sdcard/aud_stream.bin 9\n");
+	printf("	(Capture Primary Mic and Far End In (9 = 1 + 8))\n");
+	printf("Channel IDs for %s\n", fwVersion2String[AUDIENCE_ES325]);
+	printf("	Primary Mic   = %d\n", ES325_CH_PRI_MIC);
+	printf("	Secondary Mic = %d\n", ES325_CH_SEC_MIC);
+	printf("	Third Mic     = %d\n", ES325_CH_THIRD_MIC);
+	printf("	Fourth Mic    = %d\n", ES325_CH_FOURTH_MIC);
+	printf("	Far End In    = %d\n", ES325_CH_FAR_END_INPUT);
+	printf("	Audio in 1    = %d\n", ES325_CH_AUDIO_INPUT_1);
+	printf("	Audio in 2    = %d\n", ES325_CH_AUDIO_INPUT_2);
+	printf("	Audio in 3    = %d\n", ES325_CH_AUDIO_INPUT_3);
+	printf("	Audio in 4    = %d\n", ES325_CH_AUDIO_INPUT_4);
+	printf("	UI tone 1     = %d\n", ES325_CH_UI_TONE_1);
+	printf("	UI tone 2     = %d\n", ES325_CH_UI_TONE_2);
+	printf("	Spk ref 1     = %d\n", ES325_CH_SPK_REF_1);
+	printf("	Spk ref 2     = %d\n", ES325_CH_SPK_REF_2);
+	printf("	Clean Speech  = %d\n", ES325_CH_CLEAN_SPEECH);
+	printf("	Far End out 1 = %d\n", ES325_CH_FAR_END_OUT_1);
+	printf("	Far End out 2 = %d\n", ES325_CH_FAR_END_OUT_2);
+	printf("	Audio out 1   = %d\n", ES325_CH_AUDIO_OUTPUT_1);
+	printf("	Audio out 2   = %d\n", ES325_CH_AUDIO_OUTPUT_2);
+	printf("	Audio out 3   = %d\n", ES325_CH_AUDIO_OUTPUT_3);
+	printf("	Audio out 4   = %d\n", ES325_CH_AUDIO_OUTPUT_4);
+	printf("	Example: ad_streamer -c es325 -t 10 -f /sdcard/aud_stream.bin 16\n");
+	printf("	(Capture Clean Speech)\n");
+	printf("f option: output file path (default: %s)\n", DEFAULT_OUTPUT_FILE);
+	printf("t option: capture duration in seconds (default: %d)\n", DEFAULT_CAPTURE_DURATION_IN_SEC);
+	printf("c option: force chip selection in <%s|%s> (default: chip autodetection)\n", fwVersion2String[AUDIENCE_ES305], fwVersion2String[AUDIENCE_ES325]);
 }
+
+int parse_cmd_line(int argc, char** argv){
+	char *cvalue = NULL;
+	int index;
+	int c;
+
+	opterr = 0;
+
+	// Parse optional parameters
+	while ((c = getopt (argc, argv, "t:f:c:")) != -1)
+		switch (c)
+		{
+		case 'f':
+			cvalue = optarg;
+			snprintf(fname, MAX_FILE_PATH_SIZE, "%s", cvalue);
+			fname[MAX_FILE_PATH_SIZE-1]='\0';
+			break;
+		case 'c':
+			cvalue = optarg;
+			if (strncmp(cvalue, "es305", 5) == 0) {
+				fw_version = AUDIENCE_ES305;
+			} else if (strncmp(cvalue, "es325", 5) == 0) {
+				fw_version = AUDIENCE_ES325;
+				// According eS325 specifications, bit #15 of commands must be set to 1
+				setChannelCmd[2] = 0x80;
+			} else {
+				display_help();
+				printf("\n*Please use valid chip value\n");
+				return -1;
+			}
+			break;
+		case 't':
+			// Cap the duration_in_sec to capture samples (in seconds)
+			if (check_numeric(optarg)) {
+				display_help();
+				printf("\n*Please use valid numeric value for seconds\n");
+				return -1;
+			}
+			duration_in_sec = atoi(optarg);
+			break;
+		case '?':
+			if ((optopt == 'f') || (optopt == 's')) {
+				fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+			}
+		default:
+			display_help();
+			return -1;
+		}
+
+	// Non-optional arguments
+	for (index = optind; index < argc; index++){
+		if (check_numeric(argv[optind])) {
+			display_help();
+			printf("\n*Please use valid numeric value for channel flag\n");
+			return -1;
+		}
+		// Set streaming channel flag
+		channel_flag = atoi(argv[optind]);
+	}
+	return 0;
+};
+
 int main(int argc, char **argv) {
 	int rc;
 
 	unsigned char buf[READ_ACK_BUF_SIZE];
-	char fname[MAX_FILE_PATH_SIZE];
 
 	struct sched_param param;
 	pthread_attr_t thread_attr;
@@ -187,46 +339,22 @@ int main(int argc, char **argv) {
 	pthread_t pt_capture;
 	pthread_t pt_fwrite;
 
-	int duration_in_sec = 0;
-	int channel_flag = 0;
-
-	if(argc < 3){
-		display_help();
+	// Parse cmd line parameters
+	if (parse_cmd_line(argc, argv) == -1) {
 		return -1;
 	}
 
-	if(check_numeric(argv[1])){
-		display_help();
-		printf("\n*Please use valid numeric value for channel flag\n");
+	// Get the Audience FW version
+	if (get_fw_version() == -1) {
 		return -1;
-	}
-
-	if(check_numeric(argv[2])){
-		display_help();
-		printf("\n*Please use valid numeric value for seconds\n");
-		return -1;
-	}
-
-	// Set streaming channel flag
-	channel_flag = atoi(argv[1]);
-
-	// Cap the duration_in_sec to capture samples (in seconds)
-	duration_in_sec = atoi(argv[2]);
+    };
 
 	// Initialize
-	fd = open(ES305_DEVICE_PATH, O_RDWR | O_NONBLOCK, 0);
+	fd = open(ES3X5_DEVICE_PATH, O_RDWR | O_NONBLOCK, 0);
 
 	if (fd < 0) {
-		printf("Cannot open %s\n", ES305_DEVICE_PATH);
+		printf("Cannot open %s\n", ES3X5_DEVICE_PATH);
 		return -1;
-	}
-
-	// Set output file
-	if (argc > 3) {
-		snprintf(fname, MAX_FILE_PATH_SIZE, "%s", argv[3]);
-	} else {
-		strncpy(fname, DEFAULT_OUTPUT_FILE, MAX_FILE_PATH_SIZE - 1);
-		fname[MAX_FILE_PATH_SIZE-1]='\0';
 	}
 
 	outfile = fopen(fname, "w+b");
@@ -238,8 +366,8 @@ int main(int argc, char **argv) {
 	printf("Outputing raw streaming data to %s\n", fname);
 
 	// Set streaming channels
-	setChannelCmd[ES305_CHANNEL_SELECT_FIELD] = channel_flag;
-	printf("Setting streaming channels: 0x%02x\n", setChannelCmd[ES305_CHANNEL_SELECT_FIELD]);
+	setChannelCmd[ES3X5_CHANNEL_SELECT_FIELD] = channel_flag;
+	printf("Setting streaming channels: 0x%02x\n", setChannelCmd[ES3X5_CHANNEL_SELECT_FIELD]);
 
 	rc = write(fd, setChannelCmd, sizeof(setChannelCmd));
 	if (rc < 0) {

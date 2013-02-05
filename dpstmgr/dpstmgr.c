d5bbfa68a99872b9baaf609c7305854808e7459a
Geng, Xiujun <xiujun.geng@intel.com>
2013-02-05 16:24:17 +0800

    Gfx: add dpst user space support for clovertrial.
    BZ: 79041
    
    This patch add dpst user space support for clovertrial. It includes
    an application for testing DPST. And there're 3 libs. The algorithm
    version is 3, and default aggressive level is 3, and can control
    through sysfs. this must have kernel patch support. It's a reference
    implementation of DPST user sapce code.
    
    Change-Id: Ib7744d0b7f7719fa0418db0247f596ece0f30710
    Signed-off-by: Geng, Xiujun <xiujun.geng@intel.com>
    Reviewed-on: http://android.intel.com:8080/89788
    Tested-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ dpstmgr/dpstmgr.c
@@ -0,0 +1,22 @@
+#include <cutils/log.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dpstmgr.h"
+
+int main(int argc, char** argv)
+{
+	int ret = 0;
+	struct dpstmgr_cmd_hdr cmd_hdr;
+
+	ret = dpst_netlink_init();
+	if (ret) {
+		LOGE("Init netlink socket FAILD!\n");
+		return -EFAULT;
+	}
+
+	while (1) {
+		dpst_recv_from_kernel(&cmd_hdr);
+	}
+
+	return 0;
+}

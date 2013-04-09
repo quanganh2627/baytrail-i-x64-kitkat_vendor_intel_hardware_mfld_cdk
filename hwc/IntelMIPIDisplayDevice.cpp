ec9ccf6284e917d23e2907ab01e50ffe9f0dc577
Hong Liu <hong.liu@intel.com>
2013-04-09 23:10:57 +0800

    [PORT FROM 4.2]hwc: avoid posting two same buffers
    BZ: 97308
    
    Posting two same buffers consecutively to SGX may cause SGX buffer
    lockup. Adding an array to remember to last flipped buffer handle, and
    not passing them to SGX if duplicated buffer handle is found.
    
    Change-Id: I73b7861a55621c21f085d683f9855884c3eadd2c
    Signed-off-by: Hong Liu <hong.liu@intel.com>
    Reviewed-on: http://android.intel.com:8080/100607
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- hwc/IntelMIPIDisplayDevice.cpp
+++ hwc/IntelMIPIDisplayDevice.cpp
@@ -90,6 +90,8 @@ IntelMIPIDisplayDevice::IntelMIPIDisplayDevice(IntelBufferManager *bm,
     mHasSkipLayer = false;
     memset(&mLastHandles[0], 0, sizeof(mLastHandles));
 
+    memset(&mPrevFlipHandles[0], 0, sizeof(mPrevFlipHandles));
+
     mInitialized = true;
     return;
 
@@ -688,6 +690,9 @@ bool IntelMIPIDisplayDevice::prepare(hwc_display_contents_1_t *list)
 
     handleSmartComposition(list);
 
+    if (list->flags & HWC_GEOMETRY_CHANGED)
+	    memset(&mPrevFlipHandles[0], 0, sizeof(mPrevFlipHandles));
+
     return true;
 }
 
@@ -835,9 +840,27 @@ bool IntelMIPIDisplayDevice::commit(hwc_display_contents_1_t *list,
             bool ret = plane->flip(context, flags);
             if (!ret)
                 ALOGW("%s: failed to flip plane %d context !\n", __func__, i);
-            else
-                bufferHandles[numBuffers++] =
-                (buffer_handle_t)plane->getDataBufferHandle();
+            else {
+                int planeType = plane->getPlaneType();
+                buffer_handle_t handle =
+                    (buffer_handle_t)plane->getDataBufferHandle();
+
+                if ((planeType == IntelDisplayPlane::DISPLAY_PLANE_PRIMARY ||
+                     planeType == IntelDisplayPlane::DISPLAY_PLANE_SPRITE) &&
+                    (i < 10 && handle == mPrevFlipHandles[i])) {
+                    // If we post two consecutive same buffers through
+                    // primary plane, don't pass buffer handle to avoid
+                    // SGX deadlock issue
+                    ALOGD("same RGB buffer handle %p", handle);
+                } else {
+                    bufferHandles[numBuffers++] =
+                        (buffer_handle_t)plane->getDataBufferHandle();
+                    if (i < 10) {
+                        mPrevFlipHandles[i] =
+			    (buffer_handle_t)plane->getDataBufferHandle();
+                    }
+                }
+            }
 
             // clear flip flags, except for DELAY_DISABLE
             mLayerList->setFlags(i, flags & IntelDisplayPlane::DELAY_DISABLE);

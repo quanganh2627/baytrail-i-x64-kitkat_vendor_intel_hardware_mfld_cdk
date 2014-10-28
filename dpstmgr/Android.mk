f615ef215cece82532757849571816e9a542d2f9
Nie Jun <jun.nie@intel.com>
2014-10-28 16:07:28 +0800

    dpst: add user space lib
    jira: 6785
    
    dpst: add user space dpst lib to enable DPST
    
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-6785
    Change-Id: I9fab43feaed9119adf429bad6f9456edfd9ce379
    Signed-off-by: Nie Jun <jun.nie@intel.com>
    Reviewed-on: https://android.intel.com:443/293911

---
+++ dpstmgr/Android.mk
@@ -0,0 +1,20 @@
+ifeq ($(INTEL_DPST),true)
+ifneq ($(ENABLE_MRFL_GRAPHICS),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	dpstmgr.c
+
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libdm_dpst
+LOCAL_MODULE:= dpstmgr
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+include $(LOCAL_PATH)/lib/Android.mk
+
+endif
+endif
